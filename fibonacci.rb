4d1722144f372bf4d6d62b822364cd562c2b2598
lindsaymkelly <lindzlady@gmail.com>
2017-01-10 23:39:00 -0500

    Add iterative fibonacci sequence

--- fibonacci.rb
+++ fibonacci.rb
@@ -8,3 +8,20 @@ end
 p recursive_fibonacci(1)
 p recursive_fibonacci(5)
 p recursive_fibonacci(10)
+
+# Now do so with an iterative method
+
+def iterative_fibonacci(n)
+  index = 2
+  fibonacci_nums = [0,1]
+  return fibonacci_nums[n] if n < 2
+  until index > n 
+    fibonacci_nums[index] = fibonacci_nums[index-1] + fibonacci_nums[index-2]
+    index += 1
+  end
+  fibonacci_nums[n]
+end
+
+p iterative_fibonacci(1)
+p iterative_fibonacci(5)
+p iterative_fibonacci(10)
